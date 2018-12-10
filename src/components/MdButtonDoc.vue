3bff7f72c62cacb8c1466a25280f4df9ff771798
Renan Zozimo <renanfz@gmail.com>
2018-12-09 22:36:04 -0200

    Project setup + some md-button documentation parts

--- src/components/MdButtonDoc.vue
+++ src/components/MdButtonDoc.vue
@@ -1,7 +1,78 @@
 <template>
   <div>
+    <h1 class="md-display-3 s1-U__fw--300">Buttons</h1>
+
     <div>
-      <small>Flat</small>
+      <h2 class="display-2">1. Simple buttons</h2>
+      <p>
+        <b>S1CSS</b> remove margin from any button (because we believe that all spacing control need to be on dev's hands) and add new buttons (
+        <code>.md-bordered</code> and
+        <code>.md-buton-icon.squared</code>)
+      </p>
+
+      <div>
+        <h3>Flat</h3>
+        <div class="md-layout">
+          <div class="md-layout-item md-medium-size-50 md-small-size-100" id="flat">
+            <div>
+              <md-button>Default</md-button>
+              <md-button :md-ripple="false">Ripple Off</md-button>
+              <md-button class="md-primary">Primary</md-button>
+              <md-button class="md-accent">Accent</md-button>
+            </div>
+            <div>
+              <md-button disabled>Default</md-button>
+              <md-button :md-ripple="false" disabled>Ripple Off</md-button>
+              <md-button class="md-primary" disabled>Primary</md-button>
+              <md-button class="md-accent" disabled>Accent</md-button>
+            </div>
+          </div>
+          <div class="md-layout-item md-medium-size-50 md-small-size-100">
+            <div class="example-code">
+              <code>
+                &#x3C;md-button&#x3E;Default&#x3C;/md-button&#x3E;
+                &#x3C;md-button :md-ripple=&#x22;false&#x22;&#x3E;Ripple Off&#x3C;/md-button&#x3E;
+                &#x3C;md-button class=&#x22;md-primary&#x22;&#x3E;Primary&#x3C;/md-button&#x3E;
+                &#x3C;md-button class=&#x22;md-accent&#x22;&#x3E;Accent&#x3C;/md-button&#x3E;
+              </code>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h3>Raised</h3>
+        <div class="md-layout">
+          <div class="md-layout-item md-medium-size-50 md-small-size-100" id="raised">
+            <div>
+              <md-button class="md-raised">Default</md-button>
+              <md-button class="md-raised" :md-ripple="false">Ripple Off</md-button>
+              <md-button class="md-raised md-primary">Primary</md-button>
+              <md-button class="md-raised md-accent">Accent</md-button>
+            </div>
+            <div>
+              <md-button class="md-raised" disabled>Default</md-button>
+              <md-button class="md-raised" :md-ripple="false" disabled>Ripple Off</md-button>
+              <md-button class="md-raised md-primary" disabled>Primary</md-button>
+              <md-button class="md-raised md-accent" disabled>Accent</md-button>
+            </div>
+          </div>
+          <div class="md-layout-item md-medium-size-50 md-small-size-100">
+            <div class="example-code">
+              <code>
+                &#x3C;md-button class=&#x22;md-raised&#x22;&#x3E;Default&#x3C;/md-button&#x3E;
+                &#x3C;md-button class=&#x22;md-raised&#x22; :md-ripple=&#x22;false&#x22;&#x3E;Ripple Off&#x3C;/md-button&#x3E;
+                &#x3C;md-button class=&#x22;md-raised md-primary&#x22;&#x3E;Primary&#x3C;/md-button&#x3E;
+                &#x3C;md-button class=&#x22;md-raised md-accent&#x22;&#x3E;Accent&#x3C;/md-button&#x3E;
+              </code>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <h3>Flat</h3>
       <md-button>Default</md-button>
       <md-button :md-ripple="false">Ripple Off</md-button>
       <md-button class="md-primary">Primary</md-button>
@@ -10,7 +81,7 @@
     </div>
 
     <div>
-      <small>Raised</small>
+      <h3>Raised</h3>
       <md-button class="md-raised">Default</md-button>
       <md-button class="md-raised" :md-ripple="false">Ripple Off</md-button>
       <md-button class="md-raised md-primary">Primary</md-button>
@@ -19,7 +90,7 @@
     </div>
 
     <div>
-      <small>Bordered</small>
+      <h3>Bordered</h3>
       <md-button class="md-bordered">Default</md-button>
       <md-button class="md-bordered" :md-ripple="false">Ripple Off</md-button>
       <md-button class="md-bordered md-primary">Primary</md-button>
@@ -28,12 +99,13 @@
     </div>
 
     <div>
-      <small>Dense</small>
+      <h3>Dense</h3>
       <md-button class="md-dense md-primary">Flat</md-button>
       <md-button class="md-dense md-raised md-primary">Raised</md-button>
     </div>
+
     <div>
-      <small>Flat</small>
+      <h3>Flat</h3>
       <md-button class="md-icon-button">
         <md-icon>home</md-icon>
       </md-button>
@@ -52,7 +124,7 @@
     </div>
 
     <div>
-      <small>Raised</small>
+      <h3>Raised</h3>
       <md-button class="md-icon-button md-raised">
         <md-icon>home</md-icon>
       </md-button>
@@ -71,7 +143,7 @@
     </div>
 
     <div>
-      <small>Bordered</small>
+      <h3>Bordered</h3>
       <md-button class="md-icon-button md-bordered">
         <md-icon>home</md-icon>
       </md-button>
@@ -90,7 +162,7 @@
     </div>
 
     <div>
-      <small>Dense</small>
+      <h3>Dense</h3>
       <md-button class="md-icon-button md-dense md-primary">
         <md-icon>person</md-icon>
       </md-button>
@@ -101,7 +173,7 @@
     </div>
 
     <div>
-      <small>Flat</small>
+      <h3>Flat</h3>
       <md-button class="md-icon-button squared">
         <md-icon>home</md-icon>
       </md-button>
@@ -120,7 +192,7 @@
     </div>
 
     <div>
-      <small>Raised</small>
+      <h3>Raised</h3>
       <md-button class="md-icon-button squared md-raised">
         <md-icon>home</md-icon>
       </md-button>
@@ -139,7 +211,7 @@
     </div>
 
     <div>
-      <small>Bordered</small>
+      <h3>Bordered</h3>
       <md-button class="md-icon-button squared md-bordered">
         <md-icon>home</md-icon>
       </md-button>
@@ -158,7 +230,7 @@
     </div>
 
     <div>
-      <small>Dense</small>
+      <h3>Dense</h3>
       <md-button class="md-icon-button squared md-dense md-primary">
         <md-icon>person</md-icon>
       </md-button>
@@ -166,6 +238,22 @@
       <md-button class="md-icon-button squared md-dense md-raised md-primary">
         <md-icon>cached</md-icon>
       </md-button>
+    </div>
+
+    <div>
+      <h3>Buttons</h3>
+      <md-button>Default</md-button>
+      <md-button class="md-primary">Primary</md-button>
+      <md-button class="md-accent">Accent</md-button>
+      <md-button disabled>Disabled</md-button>
+    </div>
+
+    <div>
+      <h3>Links</h3>
+      <md-button :href="pageUrl">Default</md-button>
+      <md-button :href="pageUrl" class="md-primary">Primary</md-button>
+      <md-button :href="pageUrl" class="md-accent">Accent</md-button>
+      <md-button :href="pageUrl" disabled>Disabled</md-button>
     </div>
   </div>
 </template>
@@ -178,3 +266,19 @@ export default {
   }
 };
 </script>
+
+<style>
+code {
+  padding: 0;
+  border-radius: 0;
+  white-space: pre;
+}
+.example-code {
+  background-color: #fff;
+}
+
+p code {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+</style>
+
